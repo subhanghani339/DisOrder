2fc66820d9719c8b6b369b1b20aca4c64545774e
subhanghani339 <subhanghani339@gmail.com>
2020-08-05 01:13:32 +0500

    commit

---
+++ build/precache-manifest.e2fd387aad49840d280d26a9b73aa370.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8cd76dcb1b0ad581459d40d27d8722",
+    "url": "/DisOrder/index.html"
+  },
+  {
+    "revision": "a99f0a05245790ad080c",
+    "url": "/DisOrder/static/css/2.a1f65902.chunk.css"
+  },
+  {
+    "revision": "e8f115bd55b177de2db8",
+    "url": "/DisOrder/static/css/main.14089241.chunk.css"
+  },
+  {
+    "revision": "a99f0a05245790ad080c",
+    "url": "/DisOrder/static/js/2.fff57ec9.chunk.js"
+  },
+  {
+    "revision": "59a0a07934ba5e7c0fd5281c0f1901d4",
+    "url": "/DisOrder/static/js/2.fff57ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f115bd55b177de2db8",
+    "url": "/DisOrder/static/js/main.f8ddce98.chunk.js"
+  },
+  {
+    "revision": "ad2c9cee2ece248193c3",
+    "url": "/DisOrder/static/js/runtime-main.884dc752.js"
+  }
+]);
